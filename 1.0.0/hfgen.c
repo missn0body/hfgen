c836747f6f50d0d41f14ab278720f928711b8b17
missn0body <hulk128t@gmail.com>
2024-03-07 11:03:35 -0600

    Header constructing complete

--- 1.0.0/hfgen.c
+++ 1.0.0/hfgen.c
@@ -6,12 +6,13 @@
 // DEFINITIONS START HERE
 ////////////////////////////////////////////////////////////////////////////////////
 
-const char *recipe = "%s_%s_%s\n";
-const char *header_format = "#ifndef %s\n#define %s\n\n%s\n\n#endif /*%s*/";
-const char *cpp_extern_c = "#ifdef __cplusplus\nextern \"C\" {\n#endif\n\n\n\n\n\n#ifdef __cplusplus\n}\n#endif\n";
-const char *cpp_only = "#ifndef __cplusplus\n#error \"This header is to only be used with C++ code.\"\n";
-const char *ext[] = { ".h", ".hpp", ".hh" };
+const char *recipe 	 = "%s_%s_%s";
+const char *head_format  = "#ifndef %s\n#define %s\n\n%s\n#endif /*%s*/";
+const char *cpp_extern_c = "#ifdef __cplusplus\nextern \"C\" {\n#endif/*__cplusplus*/\n\n\n\n\n\n#ifdef __cplusplus\n}\n#endif/*__cplusplus*/\n";
+const char *cpp_only 	 = "#ifndef __cplusplus\n#error \"This header is to only be used with C++ code.\"\n\n\n";
+const char *ext[] 	 = { ".h", ".hpp", ".hh" };
 
+constexpr size_t header_size = 256;
 constexpr size_t buffer_size = 64;
 constexpr size_t name_size = 24;
 
@@ -25,7 +26,8 @@ typedef struct hfgen_context
 {
 	flag_t wants;
 	char *filename, *projectname, *stem, *extension;
-	char *formatted_header;
+	char *formatted_guard;
+	char *header;
 
 } hfgen;
 
@@ -35,33 +37,58 @@ static hfgen *context = nullptr;
 // FUNCTIONS START HERE
 ////////////////////////////////////////////////////////////////////////////////////
 
-void make_file(hfgen *buffer, const char *input)
+void make_file(hfgen *in, const char *file)
 {
-	if(input == nullptr || buffer == nullptr) return;
+	if(file == nullptr || in == nullptr) return;
 
 	// Input the entire filename given...
-	strncpy(buffer->stem, input, name_size);
-	strncpy(buffer->filename, input, name_size);
+	strncpy(in->stem, file, name_size);
+	strncpy(in->filename, file, name_size);
 	// Is it a C file?
-	if(!test(&buffer->wants, IS_CPP))
+	if(!test(&in->wants, IS_CPP))
 	{
-		strncpy(buffer->extension, ext[0], name_size);
+		strncpy(in->extension, ext[0], name_size);
 	}
 	// Otherwise, it's either .hh or .hpp
-	else strncpy(buffer->extension, (test(&buffer->wants, ALT_CPP)) ? ext[2] : ext[1], name_size);
-	strncat(buffer->filename, buffer->extension, name_size);
+	else strncpy(in->extension, (test(&in->wants, ALT_CPP)) ? ext[2] : ext[1], name_size);
+	strncat(in->filename, in->extension, name_size);
 }
 
-void make_guard(hfgen *buffer)
+void make_guard(hfgen *in)
 {
-	if(buffer == nullptr || buffer->projectname == nullptr ||
-	   buffer->filename == nullptr || buffer->extension == nullptr)
+	if(in == nullptr || in->projectname == nullptr ||
+	   in->filename == nullptr || in->extension == nullptr)
 		return;
 
 	// header: projectname_filename_extension
-	snprintf(buffer->formatted_header, buffer_size, recipe, buffer->projectname,
-								buffer->stem,
-								buffer->extension + 1);
+	snprintf(in->formatted_guard, buffer_size, recipe, in->projectname,
+							   in->stem,
+							   in->extension + 1);
+	return;
+}
+
+void make_header(hfgen *in)
+{
+	if(in == nullptr || in->formatted_guard == nullptr) return;
+
+	bool cpp_guard = test(&in->wants, IS_CPP);
+	bool error_guard = test(&in->wants, ONLY_CPP) && cpp_guard;
+
+	if(cpp_guard)
+	{
+		snprintf(in->header, header_size, head_format,  in->formatted_guard,
+								in->formatted_guard,
+								error_guard ? cpp_only : "",
+								in->formatted_guard);
+	}
+	else
+	{
+		snprintf(in->header, header_size, head_format,  in->formatted_guard,
+								in->formatted_guard,
+								cpp_extern_c,
+								in->formatted_guard);
+	}
+
 	return;
 }
 
@@ -72,7 +99,7 @@ void free_struct(void)
 	free(context->filename);
 	free(context->projectname);
 	free(context->extension);
-	free(context->formatted_header);
+	free(context->formatted_guard);
 	free(context);
 	return;
 }
@@ -100,11 +127,12 @@ int main(int argc, char *argv[])
 	context->projectname = malloc(name_size);
 	context->stem = malloc(name_size);
 	context->extension = malloc(6);
-	context->formatted_header = malloc(buffer_size);
+	context->formatted_guard = malloc(buffer_size);
+	context->header = malloc(header_size);
 	// And a memory check
 	if(context->filename == nullptr || context->projectname == nullptr ||
 	   context->stem == nullptr 	|| context->extension == nullptr   ||
-	   context->formatted_header == nullptr)
+	   context->formatted_guard == nullptr || context->header == nullptr )
 	{
 		perror(argv[0]);
 		exit(EXIT_FAILURE);
@@ -117,7 +145,7 @@ int main(int argc, char *argv[])
 
 	// Set up everything that the user wants
 	setbit(&context->wants, IS_CPP);
-	setbit(&context->wants, ALT_CPP);
+	//setbit(&context->wants, ALT_CPP);
 	setbit(&context->wants, ONLY_CPP);
 
 	strncpy(context->projectname, project, name_size);
@@ -125,13 +153,13 @@ int main(int argc, char *argv[])
 
 	make_file(context, file);
 	make_guard(context);
+	make_header(context);
 
 	// And print it for now
-	fprintf(stream, "-- %s\n", context->filename);
-	fprintf(stream, "-- %s\n", context->projectname);
-	fprintf(stream, "-- %s\n", context->formatted_header);
+	fprintf(stream, "%s\n", context->header);
 
 	// Clean up
 	if(stream != stdout) fclose(stream);
 	return 0;
 }
+
